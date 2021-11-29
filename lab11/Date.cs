eaf1ea957177c99ff2cb39c31c0dab84b9340529
AnReshetilova <antonina.reshetilova@gmail.com>
2021-11-30 02:08:22 +0300

    lab11

---
+++ lab11/Date.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab11
+{
+    partial class Date
+    {
+        private int day;
+        private int month;
+        private int year;
+        private readonly int id;
+        private static DateTime todayDate;
+        private static int count;
+        private const string minDate = "01.01.0001";
+
+        #region Properties
+        public int Day
+        {
+            set
+            {
+                if (value < 1 || value > 31)
+                {
+                    Console.WriteLine("Incorrect day");
+                }
+                else
+                {
+                    day = value;
+                }
+            }
+
+            get
+            {
+                return day;
+            }
+        }
+
+        public int Month
+        {
+            set
+            {
+                if (value < 1 || value > 12)
+                {
+                    Console.WriteLine("Incorrect month");
+                }
+                else
+                {
+                    month = value;
+                }
+            }
+
+            get
+            {
+                return month;
+            }
+        }
+
+        public int Year
+        {
+            set
+            {
+                if (value < 1)
+                {
+                    Console.WriteLine("Incorrect year");
+                }
+                else
+                {
+                    year = value;
+                }
+            }
+
+            get
+            {
+                return year;
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        public DateTime TodayDate
+        {
+            get
+            {
+                return todayDate;
+            }
+            private set
+            {
+                todayDate = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public string MinDate
+        {
+            get
+            {
+                return minDate;
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public Date()
+        {
+            day = 12;
+            month = 12;
+            year = 2012;
+            count++;
+            id = count * 100000000 + year * 10000 + month * 100 + day;
+        }
+
+        public Date(int day, int month, int year)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+            count++;
+            id = count * 100000000 + year * 10000 + month * 100 + day; 
+        }
+
+        public Date(int day, int month = 12)
+        {
+            Day = day;
+            Month = month;
+            Year = 2012;
+            count++;
+            id = count * 100000000 + year * 10000 + month * 100 + day;
+        }
+
+#pragma warning disable S3963 // "static" fields should be initialized inline
+        static Date()
+        {
+            Console.WriteLine("the first date created");
+            todayDate = DateTime.Now;
+            count = 0;
+        }
+#pragma warning restore S3963 // "static" fields should be initialized inline
+
+        private Date(int Count)
+        {
+            count = Count;
+        }
+        #endregion
+
+        #region Methods
+        public void GenerateDayMonth(ref int day, ref int month)
+        {
+            Random rnd = new Random();
+            day = rnd.Next(32);
+            month = rnd.Next(13);
+        }
+
+        public void GenerateYear(out int year)
+        {
+            Random rnd = new Random();
+            year = rnd.Next(2022);
+        }
+
+        public void PrintDateLongFormat()
+        {
+            string[] longMonth = {"января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа",
+            "сентября", "октября", "ноября", "декабря"};
+
+            Console.WriteLine($"{day} {longMonth[month - 1]} {year} года");
+        }
+
+        public void PrintDateShortFormat()
+        {
+            Console.WriteLine($"{day}/{month}/{year}");
+        }
+
+        public static void GetClassInfo()
+        {
+            Console.WriteLine($"today - {todayDate}" +
+                $" counter - {count}");
+        }
+        #endregion
+    }
+}
---
+++ lab11/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            string[] months = { "June", "July", "May", "December", "January", "February", "March", "April", "August", "September", "October", "November" };
+
+            var monthWithCurrentLength = months.Where(m => m.Length == n);
+            foreach(string m in monthWithCurrentLength)
+            {
+                Console.WriteLine(m);
+            }
+            Console.WriteLine();
+
+            var summerAndWinterMonth = months.Where(m => m == "June" || m == "July" || m == "August" || m == "January" || m == "December" || m == "February");
+            foreach(string m in summerAndWinterMonth)
+            { 
+                Console.WriteLine(m); 
+            }
+            Console.WriteLine();
+
+            var alphabet = months.OrderBy(m => m);
+            foreach (string m in alphabet)
+            {
+                Console.WriteLine(m);
+            }
+            Console.WriteLine();
+
+            int currentRules = months.Count(m => m.Contains("u") && m.Length >= 4);
+            Console.WriteLine(currentRules);
+
+            List<Date> date = new List<Date>();
+            date.Add(new Date(11, 12, 2001));
+            date.Add(new Date(13, 11, 2002));
+            date.Add(new Date(14, 10, 2003));
+            date.Add(new Date(15, 9, 2004));
+            date.Add(new Date(16, 9, 2005));
+            date.Add(new Date(16, 8, 2006));
+            date.Add(new Date(18, 7, 2007));
+            date.Add(new Date(19, 6, 2008));
+            date.Add(new Date(20, 5, 2009));
+            date.Add(new Date(21, 4, 2001));
+            /*список дат для заданного года;
+                список дат, которые имеют заданный месяц
+                количество дат в определённом диапазоне
+                максимальную дату
+                Первую дату для заданного дня
+           Упорядоченный список дат (хронологически)*/
+
+            int year = 2001;
+            var datesWithCurrentYear = date.Where(d => d.Year == year);
+            foreach (Date d in datesWithCurrentYear)
+            {
+                d.PrintDateShortFormat();
+            }
+            Console.WriteLine();
+
+            int month = 9;
+            var datesWithCurrentMonth = date.Where(d => d.Month == month);
+            foreach (Date d in datesWithCurrentMonth)
+            {
+                d.PrintDateShortFormat();
+            }
+            Console.WriteLine();
+
+            int minDay = 10;
+            int maxday = 15;
+            var datesWithCurrentDays = date.Where(d => d.Day >= minDay && d.Day <= maxday);
+            foreach (Date d in datesWithCurrentDays)
+            {
+                d.PrintDateShortFormat();
+            }
+            Console.WriteLine();
+
+            Date MaxDate = (date.Where(d => (d.Month + d.Year + d.Day) == date.Max(m => (m.Month + m.Year + m.Day)))).First();
+            MaxDate.PrintDateShortFormat();
+            Console.WriteLine();
+
+            int day = 16;
+            Date dateWithCurrentDay = date.Where(d => d.Day == day).First();
+            dateWithCurrentDay.PrintDateShortFormat();
+            Console.WriteLine();
+
+            var orderedDates = date.OrderBy(y => y.Year).ThenBy(m => m.Month).ThenBy(d => d.Day);
+            foreach(Date d in orderedDates)
+            {
+                d.PrintDateShortFormat();
+            }
+            Console.WriteLine();
+
+            //4 даты с годом больше 2007, объедененные с теми, где день < 15 с исключением 2009 года. Отсортированные по году
+            var myDates = (date.Where(y => y.Year > 2007).Take(4).Union(date.Where(d => d.Day < 15)).Except((date.Where(d => d.Year == 2008))).OrderBy(y => y.Year));
+            foreach (Date d in myDates)
+            {
+                d.PrintDateShortFormat();
+            }
+            Console.WriteLine();
+
+            var joinedDates = from o in orderedDates
+                              join m in myDates on o.Year equals m.Year
+                              select m;
+            foreach (Date d in joinedDates)
+            {
+                d.PrintDateShortFormat();
+            }
+        }
+    }
+}
